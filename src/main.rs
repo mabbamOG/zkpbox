c4b6ced0b8f998a07e974d22c49ec5200b60adc9
Mario A. Barbara <mariobarbara@protonmail.ch>
2023-12-21 14:55:07 +0000

    21.12.2023 (Fp64 implemented)

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,39 @@
 
 
 // 3 things need to implement now:
-// 64-bit prime Field w/ arithmetic
-// univariate interpolation & evaluation over prime fields
-// merkle tree commitments
+// 1. 64-bit prime Field w/ arithmetic
+// 2. univariate interpolation & evaluation over prime fields
+// 3. merkle tree commitments
 
-// 64-bit prime field
+// 64-bit prime field, guarantees that all operations are in the field
 #[derive(Debug, Clone, Copy)]
 struct Fp64(u64);
 impl Fp64 {
-    const PRIME_MODULUS: u64 = 18446744073709551557; // very large 64-bit prime
+    // very large 64-bit prime
+    const PRIME_MODULUS: u64 = 18446744073709551557;
+
+    // calculates the additive inverse
+    fn neg(self) -> Self {
+        Fp64(Self::PRIME_MODULUS - self.0)
+    }
+
+    // calculates the multiplicative inverse
+    fn inv(self) -> Self {
+        // Fermat's little theorem
+        self.pow(Self::PRIME_MODULUS - 2)
+    }
+
+    // modular exponentiation by repeated squaring
+    fn pow(self, exp: u64) -> Self {
+        if exp == 0 {
+            return Fp64(1);
+        }
+        if exp % 2 == 1 {
+            return self * (self * self).pow(exp >> 1);
+        } else {
+            return (self * self).pow(exp >> 1);
+        }
+    }
 }
 impl From<u64> for Fp64 {
     fn from(value: u64) -> Self {
@@ -24,25 +48,27 @@ impl From<u64> for Fp64 {
 impl std::ops::Add for Fp64 {
     type Output = Self;
     fn add(self, rhs: Self) -> Self::Output {
-        todo!();
+        let tmp: u128 = (self.0 as u128 + rhs.0 as u128) % Self::PRIME_MODULUS as u128;
+        Fp64(tmp as u64)
     }
 }
 impl std::ops::Sub for Fp64 {
     type Output = Self;
     fn sub(self, rhs: Self) -> Self::Output {
-        todo!();
+        self + rhs.neg()
     }
 }
 impl std::ops::Mul for Fp64 {
     type Output = Self;
     fn mul(self, rhs: Self) -> Self::Output {
-        todo!();
+        let tmp: u128 = (self.0 as u128 * rhs.0 as u128) % Self::PRIME_MODULUS as u128;
+        Fp64(tmp as u64)
     }
 }
 impl std::ops::Div for Fp64 {
     type Output = Self;
     fn div(self, rhs: Self) -> Self::Output {
-        todo!();
+        self * rhs.inv()
     }
 }
 
